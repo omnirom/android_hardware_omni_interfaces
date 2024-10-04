55e69055cba0ce60c1fadf27ad2d250df10b05c8
guibing <guibing@google.com>
2024-10-04 05:53:04 +0000

    Powerhal: Add sessions' jank frames into buckets.
    Put the jank frames into different buckets based on the reported
    duration of the frames. Currently dump the bucket counters and
    percentage info into dumsys log.
    
    Bug: 367724389
    Test: adb test adpf_test;
          adb shell dumpsys android.hardware.power.IPower/default.
    Flag: NONE powerhal doesn't have a flag yet
    Change-Id: I005b9ad565e1a44d0fa47083f8a7b9e36d2d213d

--- power-libperfmgr/aidl/tests/mocks/MockPowerSessionManager.h
+++ power-libperfmgr/aidl/tests/mocks/MockPowerSessionManager.h
@@ -20,6 +20,7 @@
 #include <aidl/AppDescriptorTrace.h>
 #include <aidl/AppHintDesc.h>
 #include <aidl/PhysicalQuantityTypes.h>
+#include <aidl/SessionMetrics.h>
 #include <gmock/gmock.h>
 
 namespace aidl::google::hardware::power::mock::pixel {
@@ -75,6 +76,8 @@ class MockPowerSessionManager {
                 (int64_t sessionId, impl::pixel::SessionJankyLevel jankyLevel, int32_t numOfFrames),
                 ());
     MOCK_METHOD(bool, getGameModeEnableState, (), ());
+    MOCK_METHOD(void, updateFrameBuckets,
+                (int64_t sessionId, const impl::pixel::FrameBuckets &lastReportedFrames), ());
 
     static testing::NiceMock<MockPowerSessionManager> *getInstance() {
         static testing::NiceMock<MockPowerSessionManager> instance{};
