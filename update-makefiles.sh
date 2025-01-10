RisingOS-staging/android_hardware_lineage_interfaces

interfaces: Add do_makefiles_update call for backend HIDLs
This still doesn't unbreak previous call, but whatever

Change-Id: I3267599cfc47a2d39ed480ebcc23514938a8f616

update-makefiles.sh
@@ -6,3 +6,8 @@ do_makefiles_update \
   "vendor.lineage:hardware/lineage/interfaces" \
   "android.hardware:hardware/interfaces" \
   "android.hidl:system/libhidl/transport"
+
+do_makefiles_update \
+  "lineage.hardware:hardware/lineage/interfaces/_backend" \
+  "android.hardware:hardware/interfaces" \
+  "android.hidl:system/libhidl/transport"

omnirom/android_hardware_omni_interfaces

update-makefiles.sh
@@ -6,3 +6,8 @@ do_makefiles_update \
   "vendor.lineage:hardware/omni/interfaces" \
   "android.hardware:hardware/interfaces" \
   "android.hidl:system/libhidl/transport"
+
+do_makefiles_update \
+  "lineage.hardware:hardware/omni/interfaces/_backend" \
+  "android.hardware:hardware/interfaces" \
+  "android.hidl:system/libhidl/transport"